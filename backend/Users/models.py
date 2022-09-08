43da22cac73098c34418e52827cf49f5d89ac0b6
Activ-Dave <83168255+Activ-Dave@users.noreply.github.com>
2022-09-08 18:49:59 +0200

    Add files via upload

---
+++ backend/Users/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password, **kwargs):
+        user = self.model(email=email, **kwargs)
+        user.set_password(password)
+        user.is_active=True
+        user.save()
+        return user
+
+    def createi_superuser(self, email, password, **kwargs):
+        return self.create_user(email, password, is_superuser=True, **kwargs)
+
+class User(AbstractBaseUser):
+    email = models.EmailField('Email: ', unique=True)
+    name = models.CharField('Imię: ', max_length=128)
+    surname = models.CharField('Nazwisko: ', max_length=128)
+    is_active = models.BooleanField('Aktywny: ', default=False)
+    is_superuser = models.BooleanField('Admin: ', default=False)
+
+    EMAIL_FIELD = 'email'
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name', 'surname']
+
+    objects = UserManager()
